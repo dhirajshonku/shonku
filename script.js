0a508b82fd99f43eeacb63d6262172bd88059953
dhirajshonku <dhirajftcsolar@gmail.com>
2024-07-16 09:31:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Your custom JavaScript code here
 
+// Example: Add a click event listener to the "Learn More" button
+document.getElementById("learn-more-btn").addEventListener("click", function() {
+    alert("Learn more about SHONKU!");
+});
