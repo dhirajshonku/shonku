c723d77e17727d8eb6a88fcbdc0448520aebb5d7
dhirajshonku <dhirajftcsolar@gmail.com>
2024-07-16 09:29:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
-    const chatMessages = $(".chat-messages");
-    const userInput = $("#user-input");
-    const sendBtn = $("#send-btn");
 
-    sendBtn.click(function() {
-        const userMessage = userInput.val();
-        if (userMessage !== "") {
-            // Call your conversational AI bot API here
-            // For demo purposes, we'll just append the message to the chat window
-            chatMessages.append(`<div class="chat-message">You: ${userMessage}</div>`);
-            userInput.val("");
-        }
-    });
-});
